2389fa3c45d9fe4d65b6a31b601d77f6ec6d1d8a
Arnel Locquiao <160256092+jinwoo00@users.noreply.github.com>
2024-09-30 23:00:03 +0800

    home

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const app =express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', routes);
+app.use(express.static('public'));
 
 app.listen(3000,()=>{console.log("Listening to the server on http://localhost:3000")});
