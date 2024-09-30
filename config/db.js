54fbb50330613f1540a10f77c294256788de4ed1
Arnel Locquiao <160256092+jinwoo00@users.noreply.github.com>
2024-09-30 23:58:35 +0800

    delete

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pet_db'
+});
+
+db.connect((err) => {
+    if(err)throw err;
+    console.log('connected to MySql Database.');
+});
+
+module.exports = db;
+
