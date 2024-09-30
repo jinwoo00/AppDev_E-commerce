52d0d412aec55983906460284a1c6cfdaa20dc15
Arnel Locquiao <160256092+jinwoo00@users.noreply.github.com>
2024-09-30 21:36:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.set('view engine', ejs);
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('server running on http://localhost:3000');
+});
